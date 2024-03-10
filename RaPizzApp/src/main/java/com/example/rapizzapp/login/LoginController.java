73c3dce0daf86dfa0fdf1f3094f2fd8d91744f2f
Kilouhardi <Joshual.courriel@gmail.com>
2024-03-10 19:17:16 +0100

    change window

--- RaPizzApp/src/main/java/com/example/rapizzapp/login/LoginController.java
+++ RaPizzApp/src/main/java/com/example/rapizzapp/login/LoginController.java
@@ -1,7 +1,16 @@
 package com.example.rapizzapp.login;
 
+import com.example.rapizzapp.HelloApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -19,8 +28,20 @@ public class LoginController {
     }
 
     @FXML
-    public void createAccountAction() {
-        System.out.println("hello");
+    public void createAccountAction(ActionEvent event) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(HelloApplication.class.getResource("createAccount.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Créer un compte");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            // Hide this current window (if this is what you want)
+            ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
         // Ajoutez ici la logique pour créer un compte
     }
 }
