c6d581a97ee86e8f0bb5bf84df003c76ea6ac858
Berachem Markria <berachem.markria@gmail.com>
2024-03-10 20:07:31 +0100

    bootstrap added for UI

--- RaPizzApp/src/main/java/com/example/rapizzapp/HelloApplication.java
+++ RaPizzApp/src/main/java/com/example/rapizzapp/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -12,6 +13,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        // Si on est en avance : utiliser un Boostrap Custom
+        //String css = this.getClass().getResource("/com/example/rapizzapp/styles/bootstrap.css").toExternalForm();
+        //scene.getStylesheets().add(css);        scene.getStylesheets().add(css);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
