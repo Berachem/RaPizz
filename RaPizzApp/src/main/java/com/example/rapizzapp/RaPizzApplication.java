a8a576bf8956464a00bae1f5085ff3dcbc65ad61
SAADA Zackary <ZSA4@dsone.3ds.com>
2024-06-05 18:23:25 +0200

    added application icon

--- RaPizzApp/src/main/java/com/example/rapizzapp/RaPizzApplication.java
+++ RaPizzApp/src/main/java/com/example/rapizzapp/RaPizzApplication.java
@@ -3,10 +3,12 @@ package com.example.rapizzapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class RaPizzApplication extends Application {
     @Override
@@ -15,6 +17,7 @@ public class RaPizzApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 440);
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setTitle("Bienvenue sur Rapizz ! 🍕");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/rapizz_logo.png"))));
         stage.setScene(scene);
         stage.show();
     }
